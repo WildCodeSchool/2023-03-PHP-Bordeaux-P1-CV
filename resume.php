e5f1d0842d32f7cb50de402dd5220bf8b2513a88
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-03-15 16:49:29 +0100

    Wrote the desktop skeleton of resume.php

---
+++ resume.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="ASSETS/CSS/MODULES/resume.css">
+        <title>Résumé</title>
+    </head>
+    <body class="bodyGridContainer">
+        <header>heeey</header>
+        <section id="biography">bio</section>
+        <section id="softSkillsFormation">
+            <div id="softSkills">softSkills</div>
+            <div id="formation">formation</div>
+        </section>
+        <section id="experience">exp</section>
+        <section id="contactPlan">
+            <div id="contact">contact</div>
+            <div id="plan">plan</div></section>
+        <footer>footer</footer>
+    </body>
+</html>
