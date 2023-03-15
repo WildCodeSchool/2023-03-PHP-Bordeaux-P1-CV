9ee6c86fb4a021c0eafcf8d68bb7a8553f358fc4
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-03-15 11:06:00 +0100

    Cleaned 15 files of misplaced code

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Homepage</title>
-        <link rel="stylesheet" href="ASSETS/CSS/index.css">
+        <link rel="stylesheet" href="ASSETS/CSS/main.css">
     </head>
     <body class="gridContainer">
         <header></header>
