a650bc3a05b3b6fa3e5f73df17a3811999af0675
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-03-16 11:03:02 +0100

    Started working on the experiences timeline

---
+++ _INCLUDES/experiences.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../ASSETS/CSS/MODULES/experiences.css">
+        <title>experiences</title>
+    </head>
+    <body>
+        heeey girl
+    </body>
+</html>
