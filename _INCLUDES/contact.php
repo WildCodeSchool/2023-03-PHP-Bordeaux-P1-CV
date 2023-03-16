9f180c89977a65a3f479b171bea714c8842290c3
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-03-16 19:33:39 +0100

    add form contact

---
+++ _INCLUDES/contact.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <h1>Formulaire de contact</h1>
+    <div class="formBloc">
+        <form action="/action_page.php">
+            <label for="fname">Nom & prénom</label>
+            <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom">
+
+            <label for="emailAddress">Email</label>
+            <input id="emailAddress" type="email" name="email" placeholder="Votre email">
+
+            <label for="sujet">Sujet</label>
+            <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message">
+
+            <label for="message">Message</label>
+            <textarea id="message" name="message" placeholder="Votre message" style="height:200px"></textarea>
+
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
+</div>
