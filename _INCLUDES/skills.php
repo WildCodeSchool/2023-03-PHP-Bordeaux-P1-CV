70d3c42996601f7d9f8ce164d1a22ba9794c5751
ItaanS <itaansilva00@gmail.com>
2023-03-21 13:01:09 +0100

    all branch fusion

--- _INCLUDES/skills.php
+++ _INCLUDES/skills.php
@@ -1,69 +1,80 @@
-    <h2 id="skills">Skills</h2>
+<h2 id="skills">Skills</h2>
+    
 <!--1st progress-->
-        <div class="circleWrap" id="circle1">
-            <div class="circle">
+<div class="test" id="circle1">
+    <div class="circleWrap" >
+        <div class="circle">
+                
                 <!--1st part-->
-                <div class="mask full-1">
-                    <div class="fill-1"></div>
-                </div>
-                <!--2nd part-->
-                <div class="mask half">
-                    <div class="fill-1"></div>
-                </div>
-                <!--number count-->
-                <div class="insideCircle">85%</div>
-
-
+            <div class="mask full-1">
+                <div class="fill-1"></div>
             </div>
-        </div>
+                <!--2nd part-->
+            <div class="mask half">
+                <div class="fill-1"></div>
+            </div>
+                <!--number count-->
+            <div class="insideCircle">85%</div>
 
+        </div>
+    </div>
+    <h3 id="mime">Mime</h3>
+</div>
 
 <!--2nd progress-->
-        <div class="circleWrap" id="circle2">
-                <div class="circle">
-                    <!--1st part-->
-                <div class="mask full-2">
-                    <div class="fill-2"></div>
-                </div>
-                <!--2nd part-->
-                <div class="mask half">
-                <div class="fill-2"></div>
-                </div>
-                <!--number count-->
-                <div class="insideCircle">92%</div>
-            </div>
-        </div>
+<div class="test2" id="circle2">
+    <div class="circleWrap" >
+        <div class="circle">
 
+                    <!--1st part-->
+            <div class="mask full-2">
+                <div class="fill-2"></div>
+            </div>
+                <!--2nd part-->
+            <div class="mask half">
+                <div class="fill-2"></div>
+            </div>
+                <!--number count-->
+            <div class="insideCircle">92%</div>
+        </div>
+    </div>
+    <h3 id="voleur">Voleur</h3>
+</div>
 
 <!--3rd progress-->
-        <div class="circleWrap" id="circle3">
-                <div class="circle">
+<div class="test3" id="circle3">
+    <div class="circleWrap" >
+        <div class="circle">
                     <!--1st part-->
-                <div class="mask full-3">
-                    <div class="fill-3"></div>
-                </div>
-                <!--2nd part-->
-                <div class="mask half">
-                    <div class="fill-3"></div>
-                </div>
-                <!--number count-->
-                <div class="insideCircle">65%</div>
+            <div class="mask full-3">
+                <div class="fill-3"></div>
             </div>
+                <!--2nd part-->
+            <div class="mask half">
+                <div class="fill-3"></div>
+            </div>
+                <!--number count-->
+            <div class="insideCircle">65%</div>
         </div>
-
+    </div>
+    <h3 id="peintre">Peintre</h3>
+</div>
 
 <!--4th progress-->
-        <div class="circleWrap" id="circle4">
-                <div class="circle">
+<div class="test4" id="circle4">
+    <div class="circleWrap" >
+        <div class="circle">
                     <!--1st part-->
-                <div class="mask full-4">
-                    <div class="fill-4"></div>
-                </div>
-                <!--2nd part-->
-                <div class="mask half">
+            <div class="mask full-4">
                 <div class="fill-4"></div>
-                </div>
-                <!--number count-->
-                <div class="insideCircle">28%</div>
             </div>
+                <!--2nd part-->
+            <div class="mask half">
+                <div class="fill-4"></div>
+            </div>
+                <!--number count-->
+            <div class="insideCircle">28%</div>
         </div>
+    </div>
+    <h3 id="prédateur">Prédateur</h3>
+</div>
