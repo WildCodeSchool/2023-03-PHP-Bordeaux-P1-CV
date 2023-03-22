fe8f9550808581d87499dd791685b5d79535174a
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-03-22 09:17:40 +0100

    add experiences

--- _INCLUDES/experiences.php
+++ _INCLUDES/experiences.php
@@ -1,33 +1,40 @@
 <section class="gridContainer">
-    <div id="title"><h1>Experiences</h1></div>
-    <div id="timeline"></div>
-    <div class="textContainer" id="exp1">
-        <div class="box">
-            <h1>TITLE</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class="connector"> </div>
-    </div>
-    <div class="textContainer" id="exp2">
-        <div class="connector"> </div>
-        <div class="box">
-            <h1>TITLE</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-    </div>
-    <div class="textContainer" id="exp3">
-        <div class="box">
-            <h1>TITLE</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class="connector"> </div>
-    </div>
-    <div class="textContainer" id="exp4">
-        <div class="connector"> </div>
-        <div class="box">
-            <h1>TITLE</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-    </div>
+	<div id="title"><h1>Experiences</h1></div>
+	<div id="timeline"></div>
+	<div class="textContainer" id="exp1">
+		<div class="box">
+			<h1>Peintre</h1>
+			<P>1969 - 1978</P>
+			<p>Nombreux chantier improvisé !!!!<br>
+			Peinture à l'eau à l'huile,
+			tous support (mur, plafond, sol et les gens...)</p>
+		</div>
+		<div class="connector"></div>
+	</div>
+	<div class="textContainer" id="exp2">
+		<div class="connector"></div>
+		<div class="box">
+			<h1>Militaire</h1>
+			<p>Pas longtemps</p>
+			<p>Affiche de recrutement intéressante mais sergent trè chiant...</p>
+
+		</div>
+	</div>
+	<div class="textContainer" id="exp3">
+		<div class="box">
+			<h1>Prof de sport</h1>
+			<p>1 épisode</p>
+			<p>Professeur de multisport dans les rues, coaching personnalisé.</p>
+		</div>
+		<div class="connector"></div>
+	</div>
+	<div class="textContainer" id="exp4">
+		<div class="connector"></div>
+		<div class="box">
+			<h1>Journaliste</h1>
+			<p>Plusieurs jours</p>
+			<p>Journaliste pour un magazine people, mais j'avais faim.</p>
+		</div>
+	</div>
 
 </section>
