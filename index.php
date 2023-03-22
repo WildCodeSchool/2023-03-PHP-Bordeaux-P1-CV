a1dd61a0a9099afd3648a4aaaada22037e4af747
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-03-22 16:45:54 +0100

    Cleaned index

--- index.php
+++ index.php
@@ -9,23 +9,24 @@
         <title>Homepage</title>
     </head>
     <body class="indexBodyGridContainer" id="indexBody">
-        <header id="indexHeader">
-            <h1>The Pink Panther</h1>
+        <header class="bccColorPrimary" id="indexHeader">
+            <h1 class="fontColorSecondary">The Pink Panther</h1>
         </header>
         <nav id="indexNav">
-            <button><h1><a href="resume.php">BIOGRAPHY</a></h1></button>
-            <button><h1><a href="resume.php#skills">SKILLS</a></h1></button>
-            <button><h1><a href="resume.php#formations">FORMATIONS</a></h1></button>
-            <button><h1><a href="resume.php#experiences">EXPERIENCES</a></h1></button>
-            <button><h1><a href="resume.php#contact">CONTACT ME</a></h1></button>
+            <button><h2><a href="resume.php">BIOGRAPHY</a></h2></button>
+            <button><h2><a href="resume.php#skills">SKILLS</a></h2></button>
+            <button><h2><a href="resume.php#formations">FORMATIONS</a></h2></button>
+            <button><h2><a href="resume.php#experiences">EXPERIENCES</a></h2></button>
+            <button><h2><a href="resume.php#contact">CONTACT ME</a></h2></button>
         </nav>
         <section id="pinkPanther">
             <img id="pantherDesktop"src="ASSETS/IMAGES/pinkPanther6.webp" alt="The Pink Panther Standing up, showing off his pretty website.">
             <img id="pantherMobile"src="ASSETS/IMAGES/pantherSide.png" alt="The Pink Panther Standing up, showing off his pretty website.">
+            
         </section>
         <main>
             <!--<iframe src="https://www.youtube-nocookie.com/embed/R4amWTwO1GM?controls=0&amp;start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>-->
-                <video controls autoplay width="250">
+                <video controls autoplay width=75%>
                     <source src="/ASSETS/IMAGES/pinkPanther.mp4" type="video/webm">
                 </video>
             <div>
@@ -34,8 +35,6 @@
                 <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, accusantium officiis commodi iusto hic neque, odio exercitationem id molestias ea culpa eveniet optio corporis veritatis eaque enim! Dolorum, illum? Minus.</h3>
             </div>
         </main>
-        <footer>
-            <p>Mentions Légales: Site conçu à des fins pédagogique</p>
-        </footer>
+        <footer class="bccColorPrimary"><p>Mentions legales</p></footer>
     </body>
 </html>
